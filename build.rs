e217b595edb96f71643cc6fd3ef70f5083ca8949
Furisto <24721048+Furisto@users.noreply.github.com>
2021-09-10 16:31:08 +0200

    Generate protobuf

--- build.rs
+++ build.rs
@@ -5,18 +5,6 @@ use std::{env, path::PathBuf};
 const PROTO_FILE: &str = "src/kubernetes/cri/proto/criapi.proto";
 
 fn main() -> Result<()> {
-    tonic_build::configure()
-        .out_dir("src/kubernetes/cri/api")
-        .compile(
-            &[PROTO_FILE],
-            &[&PathBuf::from(PROTO_FILE)
-                .parent()
-                .context("no path parent")?
-                .display()
-                .to_string()],
-        )
-        .context("compile CRI protocol buffers")?;
-
     Builder::new()
         .with_crate(env::var("CARGO_MANIFEST_DIR")?)
         .with_config(Config::from_file(".cbindgen.toml").map_err(|e| format_err!(e))?)
---
+++ crates/services/build.rs
@@ -0,0 +1,19 @@
+use anyhow::{Context, Result};
+use std::{path::PathBuf};
+
+const PROTO_FILE: &str = "src/cri/proto/criapi.proto";
+
+fn main() -> Result<()> {
+    tonic_build::configure()
+        .out_dir("src/cri/api")
+        .compile(
+            &[PROTO_FILE],
+            &[&PathBuf::from(PROTO_FILE)
+                .parent()
+                .context("no path parent")?
+                .display()
+                .to_string()],
+        )
+        .context("compile CRI protocol buffers")?;
+    Ok(())
+}
