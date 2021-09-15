ea4f5f4f5a171ace96ce40e7c2a64424738f516f
Furisto <24721048+Furisto@users.noreply.github.com>
2021-09-15 15:05:52 +0200

    Replace anyhow with thiserror

---
+++ crates/sandbox/src/error.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, SandboxError>;
+
+#[derive(Error, Debug)]
+pub enum SandboxError {
+    #[error("uninitialized field")]
+    Builder(#[from] derive_builder::UninitializedFieldError),
+}
